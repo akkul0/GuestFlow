501410e64a79b5dc3d154302307019dae440a820
akkul0 <94937477+akkul0@users.noreply.github.com>
2026-06-21 07:49:30 +0300

    Add files via upload

--- prisma/migrations/20260621180000_add_complaint_flags/migration.sql
+++ prisma/migrations/20260621180000_add_complaint_flags/migration.sql
@@ -1,1 +1,6 @@
+-- AlterTable (orders: isRequest + isComplaint)
+ALTER TABLE "orders" ADD COLUMN "isRequest" BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE "orders" ADD COLUMN "isComplaint" BOOLEAN NOT NULL DEFAULT false;
 
+-- CreateIndex
+CREATE INDEX "orders_hotelId_isComplaint_idx" ON "orders"("hotelId", "isComplaint");
